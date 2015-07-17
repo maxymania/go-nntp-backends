3ae526268bb542952c3fa8feb0ef5c70799b26e6
Simon DotOrg <simon.schmidt@htp-tel.de>
2015-07-17 12:02:33 +0200

    Update groupdb.go

--- groupdb/groupdb.go
+++ groupdb/groupdb.go
@@ -31,9 +31,19 @@ type GroupMeta struct{
 }
 
 type GroupDB interface{
+	// Adds a stream of groups, supplied using a channel. The stream MAY
+	// supply groups, that do exist already in the Database, as AddGroups
+	// is required to check each existing group, supplied through the channel.
 	AddGroups(src <- chan GroupMeta)
+	// Adds a group to the DB. The group MUST not exist already in the
+	// Database as AddGroup is not required to check the existence of
+	// the newsgroup.
 	AddGroup(group, descr string,state byte)
+	// Scans for all groups, that have the given prefix. 
+	// For every group, that has been read, the function cb is called.
 	Groups(prefix string,ptr *Group,cb func())
+	// Reads a single group and stores it in the supplied Group object.
+	// Returns true if the group exists.
 	Group(group string, ptr *Group) bool
 	Numberate(groups []string, id string)
 	GetArticleID(group string, num int64) string
